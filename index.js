1cfeeeb308b2f3fe6b110eec715468d26ee7e289
e.de.holanda.lima <e.de.holanda.lima@accenture.com>
2019-07-01 13:44:29 -0300

    crawler200

---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const express = require('express');
+
+var server = express();
+var papelMoeda;
+server.use(express.json());
+
+request('https://www.melhorcambio.com/cotacao/compra/euro/recife', function(err, res, body){
+  if(err){
+    console.log('Erro: ', + err);
+  }
+
+  let $ = cheerio.load(body);
+
+  papelMoeda = $('#div-especie h3 span').eq(1).text().replace(',', '.');
+
+});
+
+server.get('/', (req, res) => {
+  res.json({
+    valor: papelMoeda
+  });
+});
+
+server.listen(3333);

33f4e799b875c9c67dd289b5ced23ec433537171
e.de.holanda.lima <e.de.holanda.lima@accenture.com>
2019-07-02 13:51:37 -0300

    versao 2.0

--- index.js
+++ index.js
@@ -7,32 +7,34 @@ var server = express();
 var papelMoeda, corretoras = [];
 server.use(express.json());
 
-request('https://www.melhorcambio.com/cotacao/compra/euro/recife', function(err, res, body){
-  if(err){
-    console.log('Erro: ', + err);
-  }
+server.get('/:cidade', (req, response) => {
 
-  let $ = cheerio.load(body);
+  var { cidade } = req.params;
 
-  papelMoeda = $('#div-especie h3 span').eq(1).text().replace(',', '.');
-  
-  for (let i = 0; i < $('.holder-resultados .hover-tip.lista_corretoras .valor').length; i++) {
-    if (papelMoeda == $('.holder-resultados .hover-tip.lista_corretoras .valor').eq(i).text().split(" ")[0].split('R$')[1].replace(',', '.')){
+  request(`https://www.melhorcambio.com/cotacao/compra/euro/${cidade}`, function (err, res, body) {
+    if (err) {
+      console.log('Erro: ', + err);
+    }
+    let $ = cheerio.load(body);
+    corretoras = [];
+    papelMoeda = $('#div-especie h3 span').eq(1).text().replace(',', '.');
+
+    for (let i = 0; i < $('.holder-resultados .hover-tip.lista_corretoras .valor').length; i++) {
+      if (papelMoeda == $('.holder-resultados .hover-tip.lista_corretoras .valor').eq(i).text().split('R$')[1].substr(0, 5).replace(/\s/g, '').replace(',', '.')) {
         corretoras.push({
           nome: $('.holder-resultados .hover-tip.lista_corretoras .valor').eq(i).parent().find('.nome-corretora b').eq(0).text(),
           url: $('#site_' + i).val(),
           telefone: $('#telefone_' + i).val(),
-          avaliacao: $('#score_corretora_' + i).val() 
+          avaliacao: $('#score_corretora_' + i).val()
         })
+      }
     }
-  }
-});
-
-server.get('/', (req, res) => {
-  res.json({
-    valor: papelMoeda,
-    corretoras: corretoras
+    response.json({
+      valor: papelMoeda,
+      corretoras: corretoras
+    });
   });
+
 });
 
 server.listen(PORT);
